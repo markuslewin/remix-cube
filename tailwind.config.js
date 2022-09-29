0218473154ee1ad5f797919619a6183d29dd9013
Markus Lewin <lewinmarkus@gmail.com>
2022-09-29 12:56:50 +0200

    Add utilities for importing external design tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const configFromTokens = require("./css-utils/config-from-tokens");
+
+const colorTokens = require("./design-tokens/colors.json");
+
+const colors = configFromTokens(colorTokens);
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
+    colors,
     extend: {},
   },
   // Disables Tailwind's reset etc
