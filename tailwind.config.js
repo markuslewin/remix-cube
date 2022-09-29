3cc358c0f335cd8053bc956cc32632fdfbbc694c
Markus Lewin <lewinmarkus@gmail.com>
2022-09-29 18:28:27 +0200

    Add text size utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,23 @@ const plugin = require("tailwindcss/plugin");
 
 const configFromTokens = require("./css-utils/config-from-tokens");
 const customPropertiesFromTheme = require("./css-utils/custom-properties-from-theme");
+const pxToRem = require("./css-utils/px-to-rem");
+const valueMap = require("./css-utils/value-map");
 
 const colorTokens = require("./design-tokens/colors.json");
+const textSizeTokens = require("./design-tokens/text-sizes.json");
 
 const colors = configFromTokens(colorTokens);
+const fontSize = valueMap(configFromTokens(textSizeTokens), (size) => {
+  return `${pxToRem(size)}rem`;
+});
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     colors,
+    fontSize,
     extend: {},
   },
   // Disables Tailwind's reset etc
