5b25dfaa67fa9ab46f6f5f2f841484410e6ee134
Markus Lewin <lewinmarkus@gmail.com>
2022-10-05 19:20:30 +0200

    Add font loader

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import-ext-glob"),
     require("postcss-import"),
+    require("./css-utils/font-loader"),
     require("tailwindcss"),
     require("autoprefixer"),
     require("cssnano"),
